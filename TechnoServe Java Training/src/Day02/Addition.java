52eeccaf7bab63a5a20fe663246af2c55c67eecb
Atharva <atharvajagtap1972@gmail.com>
2022-02-28 18:49:48 +0530

    third commit

---
+++ TechnoServe Java Training/src/Day02/Addition.java
@@ -0,0 +1,13 @@
+package Day02;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3,b=4;
+		int c = a + b;
+		System.out.println(c);
+
+	}
+
+}
